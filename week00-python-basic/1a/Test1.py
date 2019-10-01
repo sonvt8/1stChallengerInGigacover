96391fb6341d89f5b35a2998a7529000e4d6dcf5
Thai Son <Vu>
2019-10-01 23:02:20 +0700

    Following test case 1a

--- week00-python-basic/1a/Test1.py
+++ week00-python-basic/1a/Test1.py
@@ -4,12 +4,13 @@
 #     -3 -1.2 0 0 4.56 78.9
 #     output
 #     -3
-
+import os
+import sys, traceback
 
 def readFile(input):
     try:
         f = open(input, mode="r")
-        return f.read().split()
+        return f.read().split(',')
     except FileNotFoundError:
         print('File %s not found' % input)
     finally:
@@ -18,12 +19,23 @@ def readFile(input):
 def writeFile(output,lst):
     f = open(output, mode="w")
     for i in range(0, len(lst)):
-        content[i] = float(lst[i])
-    f.write(str(int(min(lst))))
+        try:
+            content[i] = float(lst[i])
+        except Exception:
+            print('Invalid input: The item in the list must be a number')
+            sys.exit(1)
+    f.write(f'{int(min(lst))}')
     f.close()
 
 if __name__ == '__main__':
     # Read file
     content = readFile('input1.txt')
     # Write file
-    writeFile('output1.txt',content)
+    if (os.stat('input1.txt').st_size == 0):
+        print('Invalid input: Empty file')
+    elif len(content) < 6:
+        print('Invalid input: List of numbers should have at least 6 numbers ')
+    else:
+        writeFile('output1.txt', content)
+
+
