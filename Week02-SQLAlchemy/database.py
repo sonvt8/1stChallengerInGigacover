e190f5c0efa0775e39b5ad12e27197c89d06a763
Thai Son <Vu>
2019-10-01 21:11:08 +0700

    config.py requirement

--- Week02-SQLAlchemy/database.py
+++ Week02-SQLAlchemy/database.py
@@ -1,7 +1,23 @@
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
+import os.path
 import sys, traceback
-from config import USER, PASSWD, DB, HOST, PORT
+
+# Load configuration file
+file_exists = os.path.isfile('config.py')
+if file_exists:
+    from config import USER, PASSWD, DB, HOST, PORT
+else:
+    with open("config.py", "a+") as f:
+        config_string = f'USER    = your user\n' \
+                        f'PASSWD  = your password\n' \
+                        f'HOST    = your IP\n' \
+                        f'PORT    = your port\n' \
+                        f'DB      = your database\n'
+        f.write(config_string)
+        print("The configuration should be filled in config.py. Let's try again")
+        sys.exit(1)
+
 
 class Database():
     # replace the user, password, hostname and database according to your configuration according to your information
