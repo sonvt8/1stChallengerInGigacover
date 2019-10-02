f584550c85a760a54542c4623abe469f6d82dc1f
Trang <trangtruong16101993@gmail.com>
2019-10-02 16:59:23 +0700

    trang aQA tc03b

--- tests/test_1a.py
+++ tests/test_1a.py
@@ -32,3 +32,43 @@ class TestParallelRun(unittest.TestCase):
         with open(actual_output, 'r') as fo:
             data = fo.read().strip()
             assert data == '1'
+
+    def test_tc02(self):
+        #region make input file as https://docs.google.com/document/d/1spaSZlvmHTDarW6OnKvrKgJKRdbS-mKc/edit#bookmark=id.bn49fls8npyl
+        valid_input = '/tmp/tc02.input'
+        with open(valid_input, 'w') as f:
+            print('1, 22, 333', file=f)
+        #endregion
+        try:
+            actual_output = '/any/thing/'
+            find_min(input=valid_input, output=actual_output)
+        except Exception as e:
+            pass  # must get here
+            assert str(e) == f'Invalid input: List of numbers should have 6 numbers'
+
+    def test_tc03a(self):
+        #region make input file as https://docs.google.com/document/d/1spaSZlvmHTDarW6OnKvrKgJKRdbS-mKc/edit#bookmark=id.1a6jj95xd435
+        valid_input = '/tmp/tc03a.input'
+        with open(valid_input, 'w') as f:
+            print('1, 22, 333, 44, 555, a', file=f)
+        #endregion
+        try:
+            actual_output = '/tmp/tc03a.output'
+            find_min(input=valid_input, output=actual_output)
+        except Exception as e:
+            pass  # must get here
+            assert str(e) == f'Invalid input: The item in the list must be a number'
+
+    def test_tc03b(self):
+        #region make input file as https://docs.google.com/document/d/1spaSZlvmHTDarW6OnKvrKgJKRdbS-mKc/edit#bookmark=id.r0torxrmg9uh
+        valid_input = '/tmp/tc03b.input'
+        with open(valid_input, 'w') as f:
+            print('', file=f)
+        #endregion
+        try:
+            actual_output = '/tmp/tc03b.output'
+            find_min(input=valid_input, output=actual_output)
+        except Exception as e:
+            pass  # must get here
+            assert str(e) == f'Invalid input: Empty file'
+
