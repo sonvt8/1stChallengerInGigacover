4b96fb85d9392ac72590b6be87e637312234581c
Trang <trangtruong16101993@gmail.com>
2019-10-02 16:34:01 +0700

    trang aQA tc01

--- tests/test_1a.py
+++ tests/test_1a.py
@@ -19,3 +19,16 @@ class TestParallelRun(unittest.TestCase):
         except Exception as e:
             pass  # must get here
             assert str(e) == f'File {invalid_input} not found'
+
+    def test_tc01(self):
+        #region make input file as https://docs.google.com/document/d/1spaSZlvmHTDarW6OnKvrKgJKRdbS-mKc/edit#bookmark=id.ej3k2ud0bk6m
+        valid_input = '/tmp/tc01.input'
+        with open(valid_input, 'w') as f:
+            print('1, 22, 333, 44, 55, 66', file=f)
+        #endregion
+
+        actual_output = '/tmp/tc01.output'
+        find_min(input=valid_input, output=actual_output)
+        with open(actual_output, 'r') as fo:
+            data = fo.read().strip()
+            assert data == '1'
