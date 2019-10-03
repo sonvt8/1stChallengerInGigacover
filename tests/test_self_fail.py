7d05d23ca3510bfcc3365d3d427f696bd14af766
namgivu <namgivu@gmail.com>
2019-10-03 17:49:45 +0700

    demo test self fail

--- tests/test_self_fail.py
+++ tests/test_self_fail.py
@@ -1,22 +1,46 @@
 import unittest
 
-def f():
-    pass
-
-
 def setUpModule():    pass  # nothing here for now
 def tearDownModule(): pass  # nothing here for now
 
 
-class TestParallelRun(unittest.TestCase):
+def f_no_exception():
+    pass
+
+def f_with_exception():
+    raise Exception()
+
+
+class Test(unittest.TestCase):
 
     def setUp(self):    pass  # nothing here for now
     def tearDown(self): pass  # nothing here for now
 
-    def test_tc00(self):
+    def test00(self):
+        self.fail('Fail on purpose')
+        self.fail('We SHOULD NOT reach here')
+
+
+    def test01_f_no_exception(self):
         try:
-            f()
-            self.fail('bbbbb')
-            return
+            f_no_exception()
+            print('We SHOULD reach here as no exception from calling f_no_exception()')
         except Exception as e:
-            pass  # must get here
+            self.fail('We SHOULD NOT reach here')
+
+
+    #region f_with_exception()
+
+    def test02_f_with_exception(self):
+        try:
+            f_with_exception()
+            NOT_HERE = 'We SHOULD NOT reach here'; self.fail(NOT_HERE)  # this fail() also raise exception ie AssertError --> we have below line to check: assert str(e) != NOT_HERE - should use a better way to assert exception as test02b_f_with_exception()
+        except Exception as e:
+            assert str(e) != NOT_HERE, NOT_HERE
+            print('We SHOULD reach here as exception has already been raised by f_with_exception()')
+
+    def test02b_f_with_exception(self):
+        with self.assertRaises(Exception):
+            f_with_exception()
+
+    #endregion f_with_exception()
