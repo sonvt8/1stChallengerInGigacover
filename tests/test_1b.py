5a9b272ffdb6bb9262619593488593c8d473dd08
Trang <trangtruong16101993@gmail.com>
2019-10-03 12:00:51 +0700

    make input and output file

--- tests/test_1b.py
+++ tests/test_1b.py
@@ -1,7 +1,7 @@
 import unittest
 from week00_python_basic.challenge_1b.testRun import is_max_claim_count
-from week00_python_basic.challenge_1b.testRun import writeFile
-
+import textwrap
+import filecmp
 
 def setUpModule():    pass  # nothing here for now
 def tearDownModule(): pass  # nothing here for now
@@ -15,10 +15,11 @@ class TestParallelRun(unittest.TestCase):
     def test_tc00(self):
         #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.c94ptzuqqtop
         valid_input = '/tmp/tc00.input'
-        numbers = [0]
+        lines = textwrap.dedent('''
+            0
+        ''').strip()
         with open(valid_input, 'w') as f:
-            for number in numbers:
-                print('%i' % number, file=f)  #TODO Trang why not just print(number) ?
+            print(lines, file=f)
         #endregion
         try:
             actual_output = '/any/thing/'
@@ -31,16 +32,32 @@ class TestParallelRun(unittest.TestCase):
     def test_tc01(self):
         #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.w2a1103zyh53
         valid_input = '/tmp/tc01.input'
-        numbers = [4, 0, -22, 3, 44]
+        lines =textwrap.dedent('''
+            4
+            0
+            -22
+            3
+            44
+        ''').strip()
         with open(valid_input, 'w') as f:
-            for number in numbers:
-                print('%i' % number, file=f)  #TODO Trang why not just print(number) ?
+            print(lines, file=f)
         #endregion
+
+        #region make expected output
         actual_output = '/tmp/tc01.out'
+        expected_output = '/tmp/tc01.expected.out'
+        lines_eo = textwrap.dedent('''
+            0
+            0
+            0
+            1
+        ''').strip()
+        with open(valid_input, 'w') as feo:
+            print(lines_eo, file=feo)
+        #endregion
+
+        # run testes code
         is_max_claim_count(valid_input, actual_output)
-        with open(actual_output, 'r') as fo:
-            data = fo.read().strip()
-            expected_output = '0, 0, 0, 1'
-            assert data == expected_output.replace(', ', '\n')
 
-
+        # check for expected values
+        filecmp.cmp(actual_output, expected_output)
