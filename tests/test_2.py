f509154e9cb95e4e20b8b41d78feb3be1a8a9d00
Trang <trangtruong16101993@gmail.com>
2019-10-04 14:31:39 +0700

    trang aQA for tc01

--- tests/test_2.py
+++ tests/test_2.py
@@ -34,3 +34,33 @@ class Test(unittest.TestCase):
 
         # check for expected values
         filecmp.cmp(actual_output, expected_output)
+
+    def test_tc01(self):
+        # region make input file as https://docs.google.com/document/d/1v1FcxCLvVGZcAIKy1Q6aPCq3bevR64igyLG4O60JptE/edit#bookmark=id.4g8u3yvsoc7f
+        valid_input = '/tmp/tc01.input'
+        lines = textwrap.dedent('''
+            nricfin first_name middle_name last_name date_of_birth premium claim_count
+            2
+            S122333aG aN vaN nguyeN 1999-01-22 500 2   
+            S122333bG binh thi tran 1990-02-11 500 0
+        ''').strip()
+        with open(valid_input, 'w') as f:
+            print(lines, file=f)
+        # endregion
+
+        # region make expected output
+        actual_output = '/tmp.tc01.out'
+        expected_output = '/tmp/tc01.expected.out'
+        lines_out = textwrap.dedent('''
+            nricfin first_name middle_name last_name date_of_birth premium claim_count
+            S122333aG, An V. NGUYEN, 20, 1500
+            S122333aG, Binh T. TRAN, 29, 500
+        ''').strip()
+        with open(expected_output, 'w'):
+            print(lines_out, file=f)
+
+        # run testes code
+        insurance_policies(valid_input, actual_output)
+
+        # check for expected values
+        filecmp.cmp(actual_output, expected_output)
