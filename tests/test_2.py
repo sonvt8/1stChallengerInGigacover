8f92166a7cb6d02628c9ac45d19499daaf051bc5
Trang <trangtruong16101993@gmail.com>
2019-10-04 15:50:42 +0700

    change method name

--- tests/test_2.py
+++ tests/test_2.py
@@ -95,7 +95,7 @@ class Test(unittest.TestCase):
         # check for expected values
         filecmp.cmp(actual_output, expected_output)
 
-    def test_tc5a(self):
+    def test_tc05a(self):
         # region make input file as https://docs.google.com/document/d/1v1FcxCLvVGZcAIKy1Q6aPCq3bevR64igyLG4O60JptE/edit#bookmark=id.vn2rpimh9xvp
         valid_input = '/tmp/tc05a.input'
         lines = textwrap.dedent('''
@@ -112,7 +112,7 @@ class Test(unittest.TestCase):
             insurance_policies(valid_input, actual_output)
         assert str(ec.exception) == f'n must be a string'
 
-    def test_tc5b(self):
+    def test_tc05b(self):
         # region make input file as https://docs.google.com/document/d/1v1FcxCLvVGZcAIKy1Q6aPCq3bevR64igyLG4O60JptE/edit#bookmark=id.4or02ub25guj
         valid_input = '/tmp/tc05b.input'
         lines = textwrap.dedent('''
@@ -129,9 +129,9 @@ class Test(unittest.TestCase):
             insurance_policies(valid_input, actual_output)
         assert str(ec.exception) == f'n must have a value'
 
-    def test_tc5c(self):
+    def test_tc06a(self):
         # region make input file as https://docs.google.com/document/d/1v1FcxCLvVGZcAIKy1Q6aPCq3bevR64igyLG4O60JptE/edit#bookmark=id.avsqnj1qeba3
-        valid_input = '/tmp/tc05c.input'
+        valid_input = '/tmp/tc06a.input'
         lines = textwrap.dedent('''
             nricfin first_name middle_name last_name date_of_birth premium claim_count
             1
@@ -142,7 +142,7 @@ class Test(unittest.TestCase):
         # endregion
 
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
-            actual_output = '/tmp/tc05c.out'
+            actual_output = '/tmp/tc06a.out'
             insurance_policies(valid_input, actual_output)
         assert str(ec.exception) == f's must be a string'
 
