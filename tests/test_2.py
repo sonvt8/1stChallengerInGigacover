887ac44dd9cfde3b74c2d89267bce97c3c708d05
Trang <trangtruong16101993@gmail.com>
2019-10-04 13:45:04 +0700

    change class name

---
+++ tests/test_2.py
@@ -0,0 +1,12 @@
+import unittest
+from week00_python_basic.challenge_1a.run_1a import find_min
+
+
+def setUpModule():    pass  # nothing here for now
+def tearDownModule(): pass  # nothing here for now
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):    pass  # nothing here for now
+    def tearDown(self): pass  # nothing here for now
