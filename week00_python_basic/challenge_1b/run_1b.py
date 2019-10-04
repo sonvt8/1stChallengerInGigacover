a0246e0eda997300133aaba5d9e1733989386749
Thai Son <Vu>
2019-10-04 22:45:46 +0700

    fixed test1b following new format input (fail tc01)

--- week00_python_basic/challenge_1b/run_1b.py
+++ week00_python_basic/challenge_1b/run_1b.py
@@ -6,18 +6,16 @@ def read_file(input):
         return [x.rstrip('\n') for x in f.readlines()]
 
 def write_file(output, lst):
-    f = open(output, mode="w")
-    if (int(lst[0]) == 0):
-        f.write(f'')                                                        #tc01
-    else:
+    with open(output, mode="w") as f:
         try:
-            lst_num = [int(i) for i in lst[1].split(', ')]  # convert all strings in list to integers
+            lst_num = [int(i) for i in lst[1:]]  # convert all strings in list to integers'
+            max_num = max(lst_num)
+            lst_num[:] = [(1 if x == max_num else 0) for x in lst_num]
+            for ele in lst_num:                                                           #tc02
+                f.write(f'{ele}\n')
         except Exception:
-            raise Exception('All claim-count must be valid')                #tc04a
-        max_num = max(lst_num)
-        lst_num[:] = [(1 if x == max_num else 0) for x in lst_num]
-        #tc02
-        f.write(f'{str(lst_num)[1:-1]}')
+            raise Exception('All claim-count must be valid')                              #tc04a
+
 
 def find_max_claim(input, output):
     # Read file
@@ -25,25 +23,27 @@ def find_max_claim(input, output):
     if file_exists:
         content = read_file(input)
     else:
-        raise Exception('File %s not found' % input)                        #tc00
+        raise Exception('File %s not found' % input)                                      #tc00
 
     # Write file
-    try:
-        if os.stat(input).st_size == 0:
-            raise Exception('Invalid input: Empty file')
-        elif content[0] == '':
-            print('N must have a value')                                    #tc03b
-        elif len(content[0]) == 0:
-            print('N is empty')                                             #tc03c
-        else:
-            try:
-                #Try to convert N into interger if Claim-count exists
-                [int(i) for i in content[0]] if (content[1] in content) else print ('Claim-count is empty')
-            except Exception:
-                raise Exception('N must be an integer number')              #tc03a
-            write_file(output, content)
-    except IndexError:
-        raise Exception('Claim-count is empty')                             #tc04b
+    if os.stat(input).st_size == 0:
+        raise Exception('Invalid input: Empty file')                                      #tc03c
+    elif content[0] == '':
+        raise Exception('N must have a value')                                            #tc03b
+    else:
+        try:
+            #Try to convert N into interger if Claim-count exists
+            if (int(content[0]) == 0):
+                with open(output, mode="w") as f:
+                    f.write(f'')                                                          #tc01
+            else:
+                if (content[1] != ''):
+                    write_file(output, content)
+        except ValueError:
+            raise Exception('N must be an integer number')                                #tc03a
+        except IndexError:
+            raise Exception('Claim-count is empty')                                       #tc04b
+
 
 if __name__ == '__main__':
     find_max_claim(input='input.txt', output='output.txt')
