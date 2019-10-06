915b8f18d7afe740ecb3f8f0630085e0979ea3c0
Trang <trangtruong16101993@gmail.com>
2019-10-06 15:20:40 +0700

    update tc for test_1b

--- tests/test_1b.py
+++ tests/test_1b.py
@@ -1,7 +1,9 @@
 import unittest
-from week00_python_basic.challenge_1b.run_1b import find_max_claim
 import textwrap
 import filecmp
+
+from tests.util import write2file, get_tc_file
+from week00_python_basic.challenge_1b.run_1b import find_max_claim
 
 def setUpModule():    pass  # nothing here for now
 def tearDownModule(): pass  # nothing here for now
@@ -13,139 +15,187 @@ class Test(unittest.TestCase):
     def tearDown(self): pass  # nothing here for now
 
     def test_tc00(self):
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.7229ru1vnyi1
+        """
+
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
             invalid_input = '/any/path/not/exist'
             find_max_claim(input=invalid_input, output='any/thing')
         assert str(ec.exception) == f'File {invalid_input} not found'
 
     def test_tc01(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.c94ptzuqqtop
-        valid_input = '/tmp/tc01.input'
-        lines = textwrap.dedent('''
-            0
-        ''').strip()
-        with open(valid_input, 'w') as f:
-            print(lines, file=f)
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.c94ptzuqqtop
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
+        write2file(
+            filename=valid_input,
+            lines=textwrap.dedent('''
+                0
+            ''').strip(),
+        )
         #endregion
 
-        # region make expected output
-        actual_output = '/tmp/tc01.out'
-        expected_output = '/tmp/tc01.expected.out'
-        with open(expected_output, 'w') as fo:
-            print('', file=fo)
+        #region make expected output
+        expected_output = f'{tc_file}.expected.out'
+        write2file(
+            filename = expected_output,
+            lines    = textwrap.dedent('').strip(),
+        )
         #endregion
 
-        # run testes code
+        # run testee code
+        actual_output = f'{tc_file}.out'
         find_max_claim(valid_input, actual_output)
 
         # check for expected values
         assert filecmp.cmp(actual_output, expected_output)
 
     def test_tc02(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.w2a1103zyh53
-        valid_input = '/tmp/tc02.input'
-        lines = textwrap.dedent('''
-            4
-            0
-            -22
-            3
-            44
-        ''').strip()
-        with open(valid_input, 'w') as f:
-            print(lines, file=f)
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.w2a1103zyh53
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
+        write2file(
+            filename = valid_input,
+            lines    = textwrap.dedent('''
+                4
+                0
+                -22
+                3
+                44
+            ''').strip(),
+        )
         #endregion
 
         #region make expected output
-        actual_output = '/tmp/tc02.out'
-        expected_output = '/tmp/tc02.expected.out'
-        lines_eo = textwrap.dedent('''
-            0
-            0
-            0
-            1
-        ''').strip()
-        with open(expected_output, 'w') as feo:
-            print(lines_eo, file=feo)
+        expected_output = f'{tc_file}.expected.out'
+        write2file(
+            filename = expected_output,
+            lines    = textwrap.dedent('''
+                0
+                0
+                0
+                1
+            ''').strip(),
+        )
         #endregion
 
-        # run testes code
+        # run testee code
+        actual_output = f'{tc_file}.out'
         find_max_claim(valid_input, actual_output)
 
         # check for expected values
         assert filecmp.cmp(actual_output, expected_output)
 
     def test_tc03a(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.e3cieiz84v1i
-        valid_input = '/tmp/tc03a.input'
-        lines = textwrap.dedent('''
-            abc
-        ''').strip()
-        with open(valid_input, 'w') as f:
-            print(lines, file=f)
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.e3cieiz84v1i
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
+        write2file(
+            filename = valid_input,
+            lines    = textwrap.dedent('''
+               abc
+            ''').strip(),
+        )
         #endregion
 
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
-            actual_output = '/tmp/tc03a.out'
+            actual_output = f'{tc_file}.out'
             find_max_claim(valid_input, actual_output)
         assert str(ec.exception) == f'N must be an integer number'
 
     def test_tc03b(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.v5dmpp98umnc
-        valid_input = '/tmp/tc03b.input'
-        lines = textwrap.dedent("""
-                   \n
-        """).strip()
-        with open(valid_input, 'w') as f:
-            print(lines, file=f)
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.v5dmpp98umnc
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
+        write2file(
+            filename = valid_input,
+            lines    = textwrap.dedent('''
+               \n
+            ''').strip(),
+        )
         #endregion
 
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
-            actual_output = '/tmp/tc03b.out'
+            actual_output = f'{tc_file}.out'
             find_max_claim(valid_input, actual_output)
         assert str(ec.exception) == f'N must have a value'
 
     def test_tc03c(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.rjt0hzegdweo
-        valid_input = '/tmp/tc03c.input'
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.rjt0hzegdweo
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
         open(valid_input, 'w').close()
         #endregion
 
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
-            actual_output = '/tmp/tc03c.out'
+            actual_output = f'{tc_file}.out'
             find_max_claim(valid_input, actual_output)
         assert str(ec.exception) == f'Invalid input: Empty file'
 
     def test_tc04a(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.jfgz7yrat44n
-        valid_input = '/tmp/tc04a.input'
-        lines = textwrap.dedent('''
-            4
-            0
-            1
-            abc
-            2
-        ''').strip()
-        with open(valid_input, 'w') as f:
-            print(lines, file=f)
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.jfgz7yrat44n
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
+        write2file(
+            filename = valid_input,
+            lines    = textwrap.dedent('''
+                4
+                0
+                1
+                abc
+                2
+            ''').strip(),
+        )
         #endregion
 
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
-            actual_output = '/tmp/tc04a.out'
+            actual_output = f'{tc_file}.out'
             find_max_claim(valid_input, actual_output)
         assert str(ec.exception) == f'All claim-count must be valid'
 
     def test_tc04b(self):
-        #region make input file as https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.bfnsn71ffq4t
-        valid_input = '/tmp/tc04b.input'
-        lines = textwrap.dedent('''
-            4
-        ''').strip()
-        with open(valid_input, 'w') as f:
-            print(lines, file=f)
+        """
+        ref. https://docs.google.com/document/d/1SMjeNNPntRFNPrDqngh304hUh9P4cD0L/edit#bookmark=id.bfnsn71ffq4t
+        """
+        tc_file = get_tc_file(self)
+
+        #region make input file
+        valid_input = f'{tc_file}.input'
+        write2file(
+            filename=valid_input,
+            lines=textwrap.dedent('''
+                4
+            ''').strip(),
+        )
         #endregion
 
         with self.assertRaises(Exception) as ec:  # ec aka. exception context
-            actual_output = '/tmp/tc04b.out'
+            actual_output = f'{tc_file}.out'
             find_max_claim(valid_input, actual_output)
         assert str(ec.exception) == f'Claim-count is empty'
 
